ba5b9ca8440a132804003428317f214718f81c65
0bie <obie.egwim.git@gmail.com>
2016-06-02 23:39:57 -0700

    Updated to support altText prop

--- components/Image/index.js
+++ components/Image/index.js
@@ -2,28 +2,28 @@ import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './index.css';
 
-const Image = ({ className, size, shape, src, alt, caption, ...props}) => {
+const Image = ({ size, shape, src, altText, caption, ...props}) => {
     const imgSize = size ? styles[size] : '';
     const imgShape = shape ? styles[shape] : '';
     return (
         <figure styleName="root" className={imgSize}>
-            <img src={src} alt={alt} className={imgShape} />
+            <img src={src} alt={altText} className={imgShape} {...props} />
             {caption ? <figcaption styleName="foot">{caption}</figcaption> : null}
         </figure>
     );
 };
 
-Image.PropTypes = {
+Image.propTypes = {
     size: PropTypes.oneOf(['xsm', 'sm', 'md', 'lg', 'xl']),
     shape: PropTypes.string,
     src: PropTypes.string.isRequired,
     caption: PropTypes.string,
-    placeholder: PropTypes.string.isRequired
+    altText: PropTypes.string.isRequired
 };
 
 Image.defaultProps = {
     file: '',
-    placeholder: ''
+    altText: ''
 };
 
 export default CSSModules(Image, styles);
