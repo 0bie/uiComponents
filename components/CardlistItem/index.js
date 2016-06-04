b7a6b51db2dd4cc9328f69c40dc8a4390de22e73
0bie <obie.egwim.git@gmail.com>
2016-06-04 16:06:36 -0700

    Updated CardlistItem to support children and added comments

--- components/CardlistItem/index.js
+++ components/CardlistItem/index.js
@@ -5,36 +5,41 @@ import renderIcons from '../utils/renderIcons';
 
 class CardlistItem extends React.Component {
     static propTypes = {
+        /** @type {string} CardlistItem title */
         title: PropTypes.string.isRequired,
+        /** @type {string} CardlistItem sub-title */
         subtitle: PropTypes.string.isRequired,
+        /** @type {string} CardlistItem details */
         details: PropTypes.string.isRequired,
-        href: PropTypes.string.isRequired ,
-        image: PropTypes.string.isRequired,
+        /** @type {bool} If true, the CardlistItem will have an active state */
         isActive: PropTypes.bool.isRequired,
+        /** @type {function} Function triggered when the CardlistItem is hovered */
         handleSelect: PropTypes.func,
+        /** @type {array} Icons being rendered as call to actions */
         icons: PropTypes.array,
+        /** @type {node} The main content of the CardlistItem */
         children: PropTypes.node,
+        /** @type {object} An object mapping class names from ../Cardlist/index.css */
         styles: PropTypes.object
     };
     static defaultProps = {
-        href: '/assets/',
         isActive: false,
         icons:[
             {name: 'fave3', title: 'Favorite icon', desc: 'a favorite button', color: '#fff', size: 'sm'},
             {name: 'share', title: 'Share icon', desc: 'a share button', color: '#fff', size: 'sm'},
-            {name: 'volumeMute', title: 'Mute icon', desc: 'a mute button', color: '#fff', size: 'sm'}
+            {name: 'ellipsis', title: 'Ellipses icon', desc: 'a more options button', color: '#fff', size: 'sm'}
         ]
     };
     render() {
-        const {title, subtitle, details, href, image, icons, styles, isActive, handleSelect} = this.props;
+        const { title, subtitle, details, icons, styles, isActive, handleSelect, children, ...props } = this.props;
         const activeItem = isActive ? styles['itemActive'] : '';
         const activeDesc = isActive ? styles['descActive'] : '';
         const activeOptions = isActive ? styles['optionsActive'] : '';
         return (
-            <li styleName="item" className={activeItem} onMouseOver={handleSelect}>
+            <li styleName="item" className={activeItem} onMouseOver={handleSelect} {...props}>
                 <div styleName="content">
                     <a href="#" styleName="overlay"></a>
-                    <img src={href + image} alt=""></img>
+                    {children}
                     <div styleName="options" className={activeOptions}>
                         {renderIcons(icons)}
                     </div>
