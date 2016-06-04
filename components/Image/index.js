0fb627d268e06f9bbef1de97813b17ef042a1942
0bie <obie.egwim.git@gmail.com>
2016-06-03 23:14:35 -0700

    Removed src prop

--- components/Image/index.js
+++ components/Image/index.js
@@ -2,12 +2,12 @@ import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './index.css';
 
-const Image = ({ size, shape, src, altText, caption, ...props}) => {
+const Image = ({ size, shape, altText, caption, ...props}) => {
     const imgSize = size ? styles[size] : '';
     const imgShape = shape ? styles[shape] : '';
     return (
         <figure styleName="root" className={imgSize}>
-            <img src={src} alt={altText} className={imgShape} {...props} />
+            <img alt={altText} className={imgShape} {...props} />
             {caption ? <figcaption styleName="foot">{caption}</figcaption> : null}
         </figure>
     );
@@ -16,7 +16,6 @@ const Image = ({ size, shape, src, altText, caption, ...props}) => {
 Image.propTypes = {
     size: PropTypes.oneOf(['xsm', 'sm', 'md', 'lg', 'xl']),
     shape: PropTypes.string,
-    src: PropTypes.string.isRequired,
     caption: PropTypes.string,
     altText: PropTypes.string.isRequired
 };
