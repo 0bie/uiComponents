14df46a65f935b1362f88f70ab3911d16f638689
0bie <obie.egwim.git@gmail.com>
2016-06-04 20:00:56 -0700

    Updated to render as stateless functional component

--- components/TabSection/index.js
+++ components/TabSection/index.js
@@ -1,34 +1,36 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from '../TabSet/index.css';
 
-class TabSection extends Component {
-    static propTypes = {
-        title: PropTypes.node.isRequired,
-        children: PropTypes.node.isRequired,
-        isActive: PropTypes.bool.isRequired,
-        handleToggle: PropTypes.func,
-        styles: PropTypes.object
-    };
-    static defaultProps = {
-        title: '',
-        isActive: false
-    };
+const TabSection = ({ children, title, styles, isActive, handleToggle }) => {
+    const activeTab = isActive ? styles['tabActive'] : '';
+    const activeSection = isActive ? styles['sectionActive'] : '';
+    return (
+        <li styleName="item" className={activeTab} aria-hidden={!isActive} onClick={handleToggle}>
+            <span>{title}</span>
+            <div styleName="section" className={activeSection}>
+                {children}
+            </div>
+        </li>
+    );
+};
 
-    render() {
-        const {children, title, styles, isActive, handleToggle} = this.props;
-        const activeTab = isActive ? styles['tabActive'] : '';
-        const activeSection = isActive ? styles['sectionActive'] : '';
+TabSection.propTypes = {
+    /** @type {node} Title */
+    title: PropTypes.node.isRequired,
+    /** @type {node} Content with the tab section */
+    children: PropTypes.node.isRequired,
+    /** @type {bool} If true, the tab section will have an active state */
+    isActive: PropTypes.bool.isRequired,
+    /** @type {function} Triggered when the tab section item is clicked */
+    handleToggle: PropTypes.func,
+    /** @type {object} An object mapping class names from ../TabSet/index.css */
+    styles: PropTypes.object
+};
 
-        return (
-            <li styleName="item" className={activeTab} aria-hidden={!isActive} onClick={handleToggle}>
-                <span>{title}</span>
-                <div styleName="section" className={activeSection}>
-                    {children}
-                </div>
-            </li>
-        );
-    }
-}
+TabSection.defaultProps = {
+    title: '',
+    isActive: false
+};
 
 export default CSSModules(TabSection, styles);
