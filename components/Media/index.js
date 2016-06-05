46fd16bbfc4bb57080ed77ecc5c31f04cc18a27e
0bie <obie.egwim.git@gmail.com>
2016-06-04 19:57:32 -0700

    updated to render as stateless functional component

--- components/Media/index.js
+++ components/Media/index.js
@@ -3,30 +3,32 @@ import data from './data.json';
 import CSSModules from 'react-css-modules';
 import styles from './index.css';
 
-class Media extends React.Component {
-    static propTypes = {
-        title: PropTypes.node,
-        description: PropTypes.node,
-        children: PropTypes.node
-    };
-    static defaultProps = {
-        title: data.title,
-        description: data.description
-    };
-    render() {
-        const { children, title, description, ...props } = this.props;
-        return (
-            <div styleName="root" {...props}>
-                <div styleName="item">
-                    {children}
-                </div>
-                <div styleName="content">
-                    {title ? <h4 styleName="title">{title}</h4> : null}
-                    {description ? <div>{description}</div> : null}
-                </div>
+const Media = ({ children, title, description, ...props }) => {
+    return (
+        <div styleName="root" {...props}>
+            <div styleName="item">
+                {children}
             </div>
-        );
-    }
-}
+            <div styleName="content">
+                {title ? <h4 styleName="title">{title}</h4> : null}
+                {description ? <div>{description}</div> : null}
+            </div>
+        </div>
+    );
+};
+
+Media.propTypes = {
+    /** @type {node} Title */
+    title: PropTypes.node,
+    /** @type {node} Description */
+    description: PropTypes.node,
+    /** @type {node} Media item */
+    children: PropTypes.node
+};
+
+Media.defaultProps = {
+    title: data.title,
+    description: data.description
+};
 
 export default CSSModules(Media, styles);
