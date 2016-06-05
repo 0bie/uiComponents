81c00949482946eff2e21feac2baa9b3fab97d3c
0bie <obie.egwim.git@gmail.com>
2016-06-04 18:46:20 -0700

    Updated to render as stateless functional component

--- components/Icon/index.js
+++ components/Icon/index.js
@@ -2,34 +2,42 @@ import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './index.css';
 
-class Icon extends React.Component {
-    static propTypes = {
-        href: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        desc: PropTypes.string,
-        size: PropTypes.oneOf(['sm', 'md', 'lg']),
-        color: PropTypes.string,
-        width: PropTypes.string.isRequired,
-        height: PropTypes.string.isRequired,
-        className: PropTypes.string
-    };
-    static defaultProps = {
-        href: '/assets/sprite.svg#',
-        width: '40',
-        height: '40'
-    };
-    render() {
-        const { href, name, title, desc, size, color, width, height } = this.props;
-        const iconSize = size ? styles[size] : '';
-        return (
-            <svg styleName="icon" className={iconSize} style={{ fill: color }} width={width} height={height}>
-                <title>{title}</title>
-                <desc>{desc}</desc>
-                <use xlinkHref={href + name}></use>
-            </svg>
-        );
-    }
-}
+const Icon = ({ href, name, title, desc, size, color, width, height }) => {
+    const iconSize = size ? styles[size] : '';
+    return (
+        <svg styleName="icon" className={iconSize} style={{ fill: color }} width={width} height={height}>
+            <title>{title}</title>
+            <desc>{desc}</desc>
+            <use xlinkHref={href + name}></use>
+        </svg>
+    );
+};
+
+Icon.propTypes = {
+    /** @type {string} Relative path to the sprite */
+    href: PropTypes.string.isRequired,
+    /** @type {string} Name */
+    name: PropTypes.string.isRequired,
+    /** @type {string} Title */
+    title: PropTypes.string.isRequired,
+    /** @type {string} Description */
+    desc: PropTypes.string,
+    /** @type {array} Default sizes */
+    size: PropTypes.oneOf(['sm', 'md', 'lg']),
+    /** @type {string} Fill color */
+    color: PropTypes.string,
+    /** @type {number} Icon width */
+    width: PropTypes.number.isRequired,
+    /** @type {number} Icon height */
+    height: PropTypes.number.isRequired,
+    /** @type {string} class name */
+    className: PropTypes.string
+};
+
+Icon.defaultProps = {
+    href: '/assets/sprite.svg#',
+    width: 40,
+    height: 40
+};
 
 export default CSSModules(Icon, styles);
