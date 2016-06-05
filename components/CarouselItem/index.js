e3125ed683c4d135774ca501ff14e5657533fa90
0bie <obie.egwim.git@gmail.com>
2016-06-04 18:43:59 -0700

    Updated to render as stateless functional component

--- components/CarouselItem/index.js
+++ components/CarouselItem/index.js
@@ -2,22 +2,22 @@ import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from '../Carousel/index.css';
 
+const CarouselItem = ({ children, isActive, styles }) => {
+    const activeItem = isActive ? styles['itemActive'] : '';
+    return (
+        <li styleName="item" className={activeItem} aria-hidden={!isActive}>
+            {children}
+        </li>
+    );
+};
 
-class CarouselItem extends React.Component {
-    static propTypes = {
-        isActive: PropTypes.bool,
-        children: PropTypes.node,
-        styles: PropTypes.object
-    };
-    render() {
-        const {children, isActive, styles} = this.props;
-        const activeItem = isActive ? styles['itemActive'] : '';
-        return (
-            <li styleName="item" className={activeItem} aria-hidden={!isActive}>
-                {children}
-            </li>
-        );
-    }
-}
+CarouselItem.propTypes = {
+    /** @type {bool} If true, the CarouselItem will have an active state */
+    isActive: PropTypes.bool,
+    /** @type {node} Content of the CarouselItem */
+    children: PropTypes.node,
+    /** @type {object} An object mapping class names from ../Carousel/index.css */
+    styles: PropTypes.object
+};
 
 export default CSSModules(CarouselItem, styles);
