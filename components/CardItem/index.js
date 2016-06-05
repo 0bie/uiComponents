efa19a189075f6bd1d9b03aa8e1ac1a4ee9b4e0f
0bie <obie.egwim.git@gmail.com>
2016-06-04 18:38:40 -0700

    Updated to render as stateless functional component

--- components/CardItem/index.js
+++ components/CardItem/index.js
@@ -2,19 +2,17 @@ import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from '../Card/index.css';
 
-class CardItem extends React.Component {
-    static propTypes = {
-        /** @type {node} Content(s) of the CardItem */
-        children: PropTypes.node
-    };
-    render() {
-        const { children, ...props } = this.props;
-        return (
-            <div {...props}>
-                {children}
-            </div>
-        );
-    }
-}
+const CardItem = ({ children, ...props }) => {
+    return(
+        <div {...props}>
+            {children}
+        </div>
+    );
+};
+
+CardItem.propTypes = {
+    /** @type {node} Content(s) of the CardItem */
+    children: PropTypes.node
+};
 
 export default CSSModules(CardItem, styles);
