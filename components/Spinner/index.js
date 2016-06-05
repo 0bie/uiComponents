50f75f63a1d362165360c18530ba759b65a1fa40
0bie <obie.egwim.git@gmail.com>
2016-06-04 19:59:25 -0700

    Updated to render as stateless functional component

--- components/Spinner/index.js
+++ components/Spinner/index.js
@@ -1,27 +1,27 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './index.css';
 import g from '../global.css';
 
-class Spinner extends Component {
-    static propTypes = {
-        size: PropTypes.oneOf(['sm', 'md', 'lg'])
-    }
-    static defaultProps = {
-        size: 'lg'
-    }
-    render() {
-        const {size, ...props} = this.props;
-        return (
-            <div styleName={size} className={g.center}>
-                <div styleName="spinner" {...props}>
-                    <svg styleName="circle" viewBox="25 25 50 50">
-                     <circle styleName="path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"/>
-                    </svg>
-                </div>
+const Spinner = ({ size, ...props }) => {
+    return (
+        <div styleName={size} className={g.center} {...props}>
+            <div styleName="spinner">
+                <svg styleName="circle" viewBox="25 25 50 50">
+                 <circle styleName="path" cx="50" cy="50" r="20" fill="none" stroke-miterlimit="10"/>
+                </svg>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+Spinner.propTypes = {
+    /** @type {array} Default sizes */
+    size: PropTypes.oneOf(['sm', 'md', 'lg'])
+};
+
+Spinner.defaultProps = {
+    size: 'lg'
+};
 
 export default CSSModules(Spinner, styles);
