6e524971dbffbe8ea1d2dabf75690447fd8235b4
0bie <obie.egwim.git@gmail.com>
2016-06-04 18:48:07 -0700

    Added alt prop and comments

--- components/Image/index.js
+++ components/Image/index.js
@@ -2,27 +2,31 @@ import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './index.css';
 
-const Image = ({ size, shape, altText, caption, ...props}) => {
+const Image = ({ size, shape, alt, caption, ...props }) => {
     const imgSize = size ? styles[size] : '';
     const imgShape = shape ? styles[shape] : '';
     return (
         <figure styleName="root" className={imgSize}>
-            <img alt={altText} className={imgShape} {...props} />
+            <img alt={alt} className={imgShape} {...props} />
             {caption ? <figcaption styleName="foot">{caption}</figcaption> : null}
         </figure>
     );
 };
 
 Image.propTypes = {
+    /** @type {array} Default sizes */
     size: PropTypes.oneOf(['xsm', 'sm', 'md', 'lg', 'xl']),
+    /** @type {string} The shape of the image */
     shape: PropTypes.string,
+    /** @type {string} The image caption */
     caption: PropTypes.string,
-    altText: PropTypes.string.isRequired
+    /** @type {string} Alternate text */
+    alt: PropTypes.string.isRequired
 };
 
 Image.defaultProps = {
     file: '',
-    altText: ''
+    alt: ''
 };
 
 export default CSSModules(Image, styles);
