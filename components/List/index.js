8a08658545a7724267c03bc25007bca65b243eb4
0bie <obie.egwim.git@gmail.com>
2016-06-04 18:49:36 -0700

    Updated List to support hasHover prop

--- components/List/index.js
+++ components/List/index.js
@@ -2,17 +2,20 @@ import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './index.css';
 
-const List = ({ children, ...props }) => {
+const List = ({ children, hasHover, ...props }) => {
+    const hover = hasHover ? styles['hasHover'] : '';
     return (
-        <ul styleName="root" {...props}>
+        <ul styleName="root" className={hover} {...props}>
             {children}
         </ul>
     );
 };
 
 List.propTypes = {
+    /** @type {node} List items */
     children: PropTypes.node,
-    hover: PropTypes.bool
+    /** @type {bool} If true, the list item will have hover state */
+    hasHover: PropTypes.bool
 };
 
 export default CSSModules(List, styles);
