38f9e6dca6576a4cbec29f16a3b933c8a2c79ab6
0bie <obie.egwim.git@gmail.com>
2016-06-04 20:00:11 -0700

    Updated SubscribeForm

--- components/SubscribeForm/index.js
+++ components/SubscribeForm/index.js
@@ -3,38 +3,39 @@ import CSSModules from 'react-css-modules';
 import styles from './index.css';
 import g from '../global.css';
 
-class SubscribeForm extends React.Component {
-
-    static propTypes = {
-        placeholder: PropTypes.string.isRequired,
-        message: PropTypes.string.isRequired,
-        action: PropTypes.string.isRequired
-    };
-    static defaultProps = {
-        placeholder: 'Your Email Address',
-        message: 'An opt-in (confirmation) message',
-        action: 'Subscribe'
-    };
-    render() {
-        const {placeholder, message, action} = this.props;
-        return (
-            <div styleName="Subscribe">
-                <form styleName="form">
-                    <label styleName="label">
-                        <span className={g.a11yText}>{placeholder}</span>
-                        <input styleName="form__input" className="input u-border" type="email" name="subscribe_email" placeholder={placeholder} required/>
+const SubscribeForm = ({ placeholder, message, action }) => {
+    return (
+        <div styleName="root">
+            <form styleName="form">
+                <label styleName="label">
+                    <span className={g.a11yText}>{placeholder}</span>
+                    <input styleName="input-email" type="email" name="subscribe_email" placeholder={placeholder} required/>
+                </label>
+                <input styleName="button" type="submit" value={action}/>
+                <div styleName="message">
+                    <input styleName="checkbox" className={g.a11yText} type="checkbox" name="Subscribe_form" id="Subscribe_form" defaultChecked/>
+                    <label styleName="checkbox-label" htmlFor="Subscribe_form">
+                        {message}
                     </label>
-                    <input styleName="form__button" className="button" type="submit" value={action}/>
-                    <div styleName="form__checkbox">
-                        <input styleName="input_Subscribe" className={g.a11yText} type="checkbox" name="Subscribe_form" id="Subscribe_form" defaultChecked/>
-                        <label styleName="label_checkbox" htmlFor="Subscribe_form">
-                            {message}
-                        </label>
-                    </div>
-                </form>
-            </div>
-        );
-    }
-}
+                </div>
+            </form>
+        </div>
+    );
+};
+
+SubscribeForm.propTypes = {
+    /** @type {string} Input placeholder */
+    placeholder: PropTypes.string.isRequired,
+    /** @type {string} Opt in or confirmation message */
+    message: PropTypes.string.isRequired,
+    /** @type {string} Call to action text */
+    action: PropTypes.string.isRequired
+};
+
+SubscribeForm.defaultProps = {
+    placeholder: 'Your Email Address',
+    message: 'An opt-in (confirmation) message',
+    action: 'Subscribe'
+};
 
 export default CSSModules(SubscribeForm, styles);
