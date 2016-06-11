2ceac6d123f7af20bedc6c21beb52aa953504f08
0bie <obie.egwim.git@gmail.com>
2016-06-10 23:16:35 -0700

    Updated loading prop

--- components/Button/index.js
+++ components/Button/index.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './index.css';
 
-const Button = ({ children, size, btnStyle, loading, ...props }) => {
+const Button = ({ children, size, btnStyle, isLoading, ...props }) => {
     const isLoading = loading ? styles['loading'] : '';
     return (
         <button styleName={size} className={isLoading} {...props}>
@@ -15,7 +15,7 @@ Button.propTypes = {
     /** @type {array} Size */
     size: PropTypes.oneOf(['sm', 'md', 'lg', 'xl']),
     /** @type {bool} If true, the button will be in a loading state */
-    loading: PropTypes.bool,
+    isLoading: PropTypes.bool,
     /** @type {bool} If true, the button will be in a disabled state */
     disabled: PropTypes.bool,
     /** @type {node} Contents of the button */
