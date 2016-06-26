41058bacae50464b45dc081200a627d2bdfa6d55
0bie <obie.egwim.git@gmail.com>
2016-06-25 23:42:01 -0700

    Update <List /> to support active state

--- components/List/index.js
+++ components/List/index.js
@@ -1,21 +1,45 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import CSSModules from 'react-css-modules';
 import styles from './index.css';
+import Button from '../Button';
 
-const List = ({ children, hasHover, ...props }) => {
-    const hover = hasHover ? styles['hasHover'] : '';
-    return (
-        <ul styleName="root" className={hover} {...props}>
-            {children}
-        </ul>
-    );
-};
-
-List.propTypes = {
-    /** @type {node} List items */
-    children: PropTypes.node,
-    /** @type {bool} If true, the list item will have hover state */
-    hasHover: PropTypes.bool
-};
+class List extends Component {
+    static propTypes = {
+        /** @type {node} List items */
+        children: PropTypes.node,
+        /** @type {bool} If true, the list item will have hover state */
+        hasHover: PropTypes.bool,
+        /** @type {bool} If true, the list will have an active state */
+        initialIsActive: PropTypes.bool
+    }
+    static defaultProps = {
+        initialIsActive: false
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isActive: props.initialIsActive
+        };
+        this.handleToggleActive = this.handleToggleActive.bind(this);
+    }
+    handleToggleActive() {
+        const isActive = this.state.isActive;
+        this.setState({isActive: !isActive});
+    }
+    render() {
+        const { children, hasHover, ...props } = this.props;
+        const hover = hasHover ? styles['hasHover'] : '';
+        const isActive = this.state.isActive;
+        const activeList = isActive ? styles['active'] : '';
+        return (
+            <ul styleName="root" className={`${hover} ${activeList}`} {...props}>
+                {children}
+                <Button size="sm" onClick={this.handleToggleActive}>
+                  {isActive ? 'Show Less' : 'Show More'}
+                </Button>
+            </ul>
+        );
+    }
+}
 
 export default CSSModules(List, styles);
