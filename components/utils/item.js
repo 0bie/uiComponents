f8e64063279c375f81ed18ae80ed14d0abc35b7e
0bie <obie.egwim.git@gmail.com>
2016-07-01 11:21:23 -0700

    no-unused-vars

--- components/utils/item.js
+++ components/utils/item.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const renderItems = (items, children) => {
+export const renderItems = (items) => {
     if (items && items.length) {
         return items.map((item, i) => {
             return <li styleName="item" key={i}>
